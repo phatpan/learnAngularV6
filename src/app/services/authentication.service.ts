6b41b6890abbc043ad6053bc0620a6722582f9f4
phatcharaphan <phat.pan@gmail.com>
2018-07-15 23:18:36 +0700

    change import

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
